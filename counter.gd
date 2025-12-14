ad38d850c267610493b0683494989dcb81285c29
terraenn <infistorm@duck.com>
2025-12-14 13:00:07 +0200

    Counter improvements

--- counter.gd
+++ counter.gd
@@ -1,78 +1,104 @@
 @tool
-## [b]A [Label] that is meant to only store a number.[/b]
-## [br]To use, set the amount property instead of text.
+## A [Label] that is meant to only store a number.
+## [br]To use, set the [member amount] property instead of [member Label.text].
 class_name Counter extends Label
 
 #region VARIABLES & SIGNALS
 # EXPORTS ---------
-## How many decimals should be displayed.
-@export_enum("0 (display as int)", "1", "2", "3", "Infinite") var max_decimals : int = 1:
-	set(value):
-		max_decimals = value
-		displayed_amount = displayed_amount # update display
+## How many decimals should be displayed max.
+## The current, actual, amount.
 @export var amount : float:
 	set(value):
 		var old_value := amount
 		amount = value
-		animate_amount(value, old_value)
-## Whether there should be a counting up/down animation on counter amount change above [member animation_threshold]
+		# set displayed amount
+		if animate_change and absf(value - old_value) > animation_threshold:
+			set_amount_animated(value, old_value)
+		else:
+			displayed_amount = value
+@export_enum("0 (display as int)", "1", "2", "3", "Infinite") var max_decimals : int = 1:
+	set(value):
+		max_decimals = value
+		displayed_amount = displayed_amount # update display 
+## Whether there should be a counting up/down animation on counter amount change above [member animation_threshold].
 @export var animate_change : bool = false
 @export_group("Animation", "animation")
-## How much should the counter increase/decrease by to trigger an animation
+## How much should the [member amount] increase/decrease by to trigger an animation.
 @export var animation_threshold : int = 1
+## The ease the animation uses.
 @export var animation_ease : Tween.EaseType = Tween.EaseType.EASE_IN
+## The transition type the animation uses.
 @export var animation_trans : Tween.TransitionType = Tween.TransitionType.TRANS_LINEAR
+## Used in the formula for the duration of the animation.
 @export var animation_speed_multiplier : float = 0.25
+## Used in the formula for the duration of the animation.
 @export_range(0.0, 1.0, 0.05, "or_greater") var animation_min_duration : float = 0.2
+## Used in the formula for the duration of the animation.
 @export_range(0.5, 2.0, 0.05, "or_greater", "or_less") var animation_max_duration : float = 2.0
-## Displayed before [displayed_amount]
-@export var prefix : String
-## Displayed after [displayed_amount]
-@export var suffix : String
-# REGULAR ---------
-var displayed_amount : float:
+## Displayed before [member displayed_amount]
+@export var prefix : String:
 	set(value):
-		displayed_amount = value
+		prefix = value
+		displayed_amount = displayed_amount # update text
+## Displayed after [member displayed_amount]
+@export var suffix : String:
+	set(value):
+		suffix = value
+		displayed_amount = displayed_amount # update text
+## Whether anything should be displayed when amount is 0.
+@export var hide_on_zero : bool = false:
+	set(value):
+		hide_on_zero = value
+		displayed_amount = displayed_amount # update text
+# REGULAR ---------
+## The number that's displayed right now. ([int] or [float])
+var displayed_amount : Variant:
+	set(value):
 		@warning_ignore("incompatible_ternary") # intentional
-		text = str(roundi(value) if max_decimals == 0 else snappedf(value, pow(0.1, max_decimals)))
+		displayed_amount = roundi(value) if max_decimals == 0 else snappedf(value, pow(0.1, max_decimals))
+		if hide_on_zero and is_equal_approx(floorf(displayed_amount), 0):
+			text = ""
+			return
+		text =\
+		prefix + str(displayed_amount) + suffix
+## Used to reset animation when amount is changed again
 var tween : Tween
+## Used for rounding.
 var snap_step : float = pow(0.1, max_decimals) 
 # SIGNALS ---------
+## Emitted when the counting animation is triggered, i.e. when [method set_amount_animated] is called.
 signal animation_triggered
+## Emitted when the counting animation is finished, i.e. at the end of [method set_amount_animated].
 signal animation_ended
 # -----------------
 #endregion
 
-#region BUILT-IN
-func _process(_delta : float) -> void:
-	pass
-#endregion
-
 #region CLASS
+## Set the amount without an animation.
 func set_amount_directly(value : float) -> void:
 	var toggle_animate_change := animate_change
 	animate_change = false
 	amount = value
 	if toggle_animate_change:
 		animate_change = true
-
+  
 # Using a method instead of a setter because setters can't be overwritten by subclasses
 # This way subclasses can change the tweening logic if need be
 # As of Godot 4.5 at least
-## Tween the displayed amount from an old to a new value
-func animate_amount(value : float, old_value : float) -> void:
+## Tween the displayed amount from an old to a new value.
+func set_amount_animated(value : float, old_value : float) -> void:
 	var change : float = abs(old_value - value)
-	var displayed_new_value : float = snappedf(value, snap_step)
-	if animate_change and change > animation_threshold:
-		animation_triggered.emit()
-		if tween:
-			tween.kill()
-		tween = create_tween()
-		tween\
-		.tween_property(self, "displayed_amount", value, clamp(change * animation_speed_multiplier, animation_min_duration, animation_max_duration))\
-		.set_trans(animation_trans)\
-		.set_ease(animation_ease)
-		tween.tween_callback(animation_ended.emit)
-	else:
-		displayed_amount = displayed_new_value
+	animation_triggered.emit()
+	if tween:
+		tween.kill()                                                               
+	tween = create_tween()
+	tween\
+	.tween_property(self, "displayed_amount", value, clamp(change * animation_speed_multiplier, animation_min_duration, animation_max_duration))\
+	.set_trans(animation_trans)\
+	.set_ease(animation_ease)
+	tween.tween_callback(animation_ended.emit)
+
+## Update [member text] to be [member displayed_amount].
+func update_text() -> void:
+	displayed_amount = displayed_amount
 #endregion
