c738f6be61519ebe222388ab74291df875381ea8
terraenn <infistorm@duck.com>
2026-01-03 19:59:48 +0200

    Pulser changes

--- pulser.gd
+++ pulser.gd
@@ -4,20 +4,25 @@ class_name Pulser extends Node
 #region VARIABLES
 var time : float = 0
 # EXPORTS
-@export var pulse : bool = true
+@export_group("Pulse", "pulse_")
+@export_custom(PROPERTY_HINT_GROUP_ENABLE, "") var pulse_ : bool = true
 @export var pulse_from : float = 0
 @export var pulse_to : float = 1
-@export var pulse_speed : float = 1
+@export_range(0.1, 5, 0.1, "or_greater", "or_less") var pulse_speed : float = 1
 ## The node path of the pulsed property relative to this Pulser's parent.
 ## E.g. - "position", "scale:x"
-@export var property : String
+@export var property : String:
+	set(value):
+		if name == "Pulser" or name == property.capitalize().validate_node_name() + "Pulser":
+			name = value.capitalize() + "Pulser"
+		property = value
 # ONREADY
 @onready var node : Node = get_parent()
 #endregion
 
 func _process(delta: float) -> void:
 	time += delta
-	if node and pulse:
+	if node and pulse_:
 		var foo : float = get_sine(pulse_from, pulse_to, pulse_speed)
 		#node.scale = Vector2(foo, foo)
 		var cur_val : Variant = node.get_indexed(NodePath(property).get_as_property_path())
@@ -27,6 +32,8 @@ func _process(delta: float) -> void:
 			node.set_indexed(NodePath(property).get_as_property_path(), Vector2(foo, foo))
 		elif cur_val is Vector2i:
 			node.set_indexed(NodePath(property).get_as_property_path(), Vector2i(roundi(foo), roundi(foo)))
+		elif cur_val is Color:
+			node.set_indexed(NodePath(property).get_as_property_path(), Color(foo, foo, foo, foo))
 
 #func _get_property_list() -> Array[Dictionary]:
 	#var result : Array[Dictionary]
