6e53d9c00a3d7c4b5d065b1cca4df74a4959ce04
terraenn <infistorm@duck.com>
2026-01-04 12:22:18 +0200

    Added presets

--- pulser.gd
+++ pulser.gd
@@ -11,6 +11,10 @@ var time : float = 0
 @export_range(0.1, 5, 0.1, "or_greater", "or_less") var pulse_speed : float = 1
 ## The node path of the pulsed property relative to this Pulser's parent.
 ## E.g. - "position", "scale:x"
+@export var preset : Preset = Preset.CUSTOM:
+	set(value):
+		preset = value
+		notify_property_list_changed()
 @export var property : String:
 	set(value):
 		if name == "Pulser" or name == property.capitalize().validate_node_name() + "Pulser":
@@ -20,11 +24,23 @@ var time : float = 0
 @onready var node : Node = get_parent()
 #endregion
 
+enum Preset {
+	ALPHA,
+	SCALE,
+	CUSTOM,
+}
+
 func _process(delta: float) -> void:
 	time += delta
 	if node and pulse_:
 		var foo : float = get_sine(pulse_from, pulse_to, pulse_speed)
 		#node.scale = Vector2(foo, foo)
+		if preset == Preset.SCALE:
+			node.scale = Vector2(foo, foo)
+			return
+		elif preset == Preset.ALPHA:
+			node.modulate.a = foo
+			return
 		var cur_val : Variant = node.get_indexed(NodePath(property).get_as_property_path())
 		if cur_val is float:
 			node.set_indexed(NodePath(property).get_as_property_path(), foo)
@@ -34,6 +50,13 @@ func _process(delta: float) -> void:
 			node.set_indexed(NodePath(property).get_as_property_path(), Vector2i(roundi(foo), roundi(foo)))
 		elif cur_val is Color:
 			node.set_indexed(NodePath(property).get_as_property_path(), Color(foo, foo, foo, foo))
+
+func _validate_property(property_dict: Dictionary) -> void:
+	if property_dict.name == "property":
+		if preset == Preset.CUSTOM:
+			property_dict.usage |= PROPERTY_USAGE_DEFAULT
+		else:
+			property_dict.usage |= PROPERTY_USAGE_READ_ONLY
 
 #func _get_property_list() -> Array[Dictionary]:
 	#var result : Array[Dictionary]
